22a2dbe392065eaf3fafe1780f6f0c0e48283db3
T-Kingston <thomasjkingston@icloud.com>
2022-07-15 08:59:50 +0000

    add if/else statement to check status if to run game or not.

--- run.py
+++ run.py
@@ -6,3 +6,15 @@ print("---------------------------------")
 print(f"Hey, {username}! Are you ready to test your country knowledge skills?")
 print("---------------------------------")
 print("Note: answers should be 'a', 'b', or 'c' in lowercase. 'True/False' are case\n sensitive.")
+
+#Checks to see whether user wants to start quiz or not
+start_quiz = input("Type 'y' or 'n' to start: ")
+
+if start_quiz == "y":
+    print("Brilliant! Get your quiz hat on!")
+    print("Hang on a sec whilst I think of what questions to ask you...")
+    print("Ok, ready? No pressure but I thought it'd be fun to time you as well. Good luck!")
+    print("---------------------------------")
+else:
+    print("Oh... I guess this is goodbye then :(")
+    raise SystemExit()
