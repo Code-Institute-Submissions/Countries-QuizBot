38be6e9a211e0097b32583ae239e0edeaa29f571
T-Kingston <thomasjkingston@icloud.com>
2022-07-15 08:57:09 +0000

    add print statment to introduce game and prompt for username input

---
+++ run.py
@@ -0,0 +1,8 @@
+#Introduces the game and prompts user to create a username.
+print("Hi! I'm QuizBot. Create a username to start.")
+print("---------------------------------")
+username = input("Username: ")
+print("---------------------------------")
+print(f"Hey, {username}! Are you ready to test your country knowledge skills?")
+print("---------------------------------")
+print("Note: answers should be 'a', 'b', or 'c' in lowercase. 'True/False' are case\n sensitive.")
